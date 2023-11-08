493671138a9c4bbc063fca46cca0cef3d38b4a8c
Joeun99 <132464084+Joeun99@users.noreply.github.com>
2023-11-09 00:53:50 +0900

    사용자 정보 수정
    비밀번호를 너무 쉽게 설정하면 '비밀번호 유출위험' 창이 뜨기 때문에

--- Project_Login/admin_data.py
+++ Project_Login/admin_data.py
@@ -23,10 +23,11 @@ with app.app_context():
     db.create_all()
 
     # 사용자 ID와 비밀번호 리스트
-    users = [('admin1', '1111'), ('admin2', '2222'), ('admin3', '3333'), 
-             ('admin4', '4444'), ('admin5', '5555'), ('admin6', '6666'), 
-             ('admin7', '7777'), ('admin8', '8888'), ('admin9', '9999')]
-
+    users = [('admin1', 'hello111'), ('admin2', 'hello222'), ('admin3', 'hello333'), 
+             ('admin4', 'hello444'), ('admin5', 'hello555'), ('admin6', 'hello666'), 
+             ('admin7', 'hello777'), ('admin8', 'hello888'), ('admin9', 'hello999')]
+    
+    
     # 각 사용자에 대해 Admin 객체를 생성하고 데이터베이스에 추가
     for user_id, password in users:
         admin = Admin(id=user_id)
