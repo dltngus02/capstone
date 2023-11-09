74371804d7e8512ec83f6394ccf36cd023f2f665
Joeun99 <132464084+Joeun99@users.noreply.github.com>
2023-11-09 23:26:32 +0900

    전체적인 구조 수정(app.py, forms.py 추가) 및 로그인 기능 보완
    flask_login 라이브러리를 사용하여 사용자 세션을 관리
    WTForms를 이용하여 로그인 폼 구현 및 유효성 검사 수행
    로그인 성공/실패 여부에 따라 사용자에게 알림 창 띄워줌

---
+++ Project_Login(수정)/controller/controllers.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, jsonify, request
+from detect import detect_objects
+from flask_socketio import emit
+from app import db, socketio
+from DB_models.model import AutoBill
+from common.config import basic_path
+
+
+bp = Blueprint('shopping',
+                __name__,
+                url_prefix='/')
+
+@bp.route('/')
+def main():
+    return render_template('main.html')
+
+@bp.route('/장바구니')
+def shopping():
+    return render_template('shopping.html')
+
+@bp.route('/결제')
+def payment():
+    return render_template('payment.html')
+
+@bp.route('/결제완료')
+def thank():
+    return render_template('thank.html')
+
+@bp.route('/get_products')
+def get_products():
+    product_list = []
+    for class_ids in detect_objects():
+        for class_id in class_ids:
+            product = get_thing_by_id(class_id)
+            if product:
+                product_data = {
+                    "id": product.id,
+                    "name": product.name,
+                    "price": product.price,
+                    "image": basic_path + product.image.split('/')[-1],
+                    "num": product.num
+                }
+                product_list.append(product_data)
+                socketio.emit('update_product', product_data)
+    return jsonify(item=product_list)
+
+def get_thing_by_id(id):
+    return AutoBill.query.get(id)
+
